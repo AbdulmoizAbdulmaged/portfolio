d29ec9d454cd780d5f2c9eaf3c9989ee5ed24280
AbdulmoizAbdulmaged <149574102+AbdulmoizAbdulmaged@users.noreply.github.com>
2024-02-26 15:36:32 +0300

    second commit

--- ECOMMERCEAPI/index.js
+++ ECOMMERCEAPI/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+const paymentRoute = require('./routes/stripe');
+
 
 const mongoose = require('mongoose');
 
@@ -16,10 +22,16 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log('DBConnection successfully up');})
 .catch((err)=>{console.log(err);});
 
+app.use(cors());
 //create an API
 app.use(express.json());
+
 app.use('/api/users', userRoute);
 app.use('/api/auth',authRoute);
+app.use('/api/products',productRoute);
+app.use('/api/carts',cartRoute);
+app.use('/api/orders',orderRoute);
+app.use('/api/checkout',paymentRoute);
 
 
 app.listen(process.env.PORT || 5000,()=>{
